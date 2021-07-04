657be48e6cd38d83dce8581add0e8f613a445b93
MRonald <ronaldmichael918@gmail.com>
2021-07-04 11:38:07 -0300

    Melhorando a estrutura do novo banco de dados | Comentando o DUMP do novo banco

--- _dumps/sql-novo.sql
+++ _dumps/sql-novo.sql
@@ -6,32 +6,35 @@ use projeto_php_estruturado;
 
 drop table if exists cliente;
 
+# Tabela CLIENTE
+# Apenas o email é opcional
 create table if not exists cliente(
 	id int not null auto_increment primary key,
-    nome_cliente varchar(100) not null,
+    nome_cliente varchar(255) not null,
     cpf char(11) not null,
-    email nchar(10)
+    email varchar(100)
 );
 
 drop table if exists produto;
 
+# Tabela PRODUTO
 create table if not exists produto(
 	id int not null auto_increment primary key,
-    cod_barras varchar(20) not null,
-    nome_produto varchar(100),
-    valor_unitario decimal(8,2),
-    quantidade int not null
+    cod_barras varchar(40) not null,
+    nome_produto varchar(255) not null,
+    valor_unitario decimal(8,2) not null
 );
 
 drop table if exists pedido;
 
+# Tabela PEDIDO
+# Relação entre as tabelas CLIENTE e PRODUTO
 create table if not exists pedido(
-	id int not null auto_increment primary key,
-    numero_pedido int not null,
-    dt_pedido datetime not null,
+    numero_pedido int not null auto_increment primary key,
+    data_pedido datetime not null,
     id_cliente int not null,
-    id_produtos int not null,
+    id_produto int not null,
+    quantidade int not null,
     foreign key (id_cliente) references cliente (id),
-    foreign key (produto) references produto (id)
+    foreign key (id_produto) references produto (id)
 );
-
